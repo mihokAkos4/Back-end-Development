70380f43d7d7e7b43ccb05a232d6aff3b3741ab4
Ákos <mihok.akos4@gmail.com>
2025-01-12 16:51:48 +0200

    Django

---
+++ Django/Little Lemon Web Application/readme.txt
@@ -0,0 +1,18 @@
+Feel free to test these endpoints!☺
+/admin							Username: admin				Password: admin@789!
+/auth
+/restuarant/booking/
+about/
+book/
+reservations/
+menu/
+menu_item/1
+menu-items/
+menu-items/1
+auth/token/login
+auth/token/logout
+message/
+
+
+
+
---
+++ Django/Little Lemon Web Application/restaurant/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('book/', views.book, name="book"),
+    path('reservations/', views.reservations, name="reservations"),
+    path('menu/', views.menu, name="menu"),
+    path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"), 
+    path('menu-items/', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()), 
+    path('api-token-auth/', obtain_auth_token),
+
+]
